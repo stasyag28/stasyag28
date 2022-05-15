06ecab9518d21e6cf339685cf0be58ab7f1e6509
stasyag28 <stasyag28@gmail.>
2022-05-15 22:29:27 +0300

    add change

---
+++ src/com/hillel/homework2/Map.java
@@ -0,0 +1,15 @@
+package com.hillel.homework2;
+
+public class Map {
+    public static void main(String[] args) {
+
+        System.out.println("Я в Киеве");
+
+        int latitude = 50_4546600;
+        System.out.println("широта - " + latitude);
+
+        int longitude = 30_5238000;
+        System.out.println("долгота - " + longitude);
+
+    }
+}
