730cc1103923c281a71c4aed9ae1c86e71814361
stasyag28 <stasyag28@gmail.>
2022-05-20 14:15:23 +0300

    add change

--- src/com/hillel/homework4/Task2.java
+++ src/com/hillel/homework4/Task2.java
@@ -9,23 +9,23 @@ public class Task2 {
         int portionsPerDay = 3;
         int moth = 30;
         int manyPortion = 2;
-        int catFood = AnimalsFood(cat, 50, 3, 30);
+        int catFood = animalsFood(cat, 50, 3, 30);
         System.out.println("Cat food per month - " + catFood);
-        int dogFood = AnimalsFood(dog, 50, 3, 30);
+        int dogFood = animalsFood(dog, 50, 3, 30);
         System.out.println("Dog food per month - " + dogFood);
-        int sum = PortionMonth(catFood, dogFood, 50);
+        int sum = portionMonth(catFood, dogFood, 50);
         System.out.println("Money per month for the maintenance of the shelter - " + sum * manyPortion);
 
 
     }
 
-    public static int AnimalsFood(int animals, int portionsPerDay, int portion, int month) {
+    public static int animalsFood(int animals, int portionsPerDay, int portion, int month) {
         return (animals * portion * portionsPerDay * month);
 
 
     }
 
-    public static int PortionMonth(int dogFood, int catFood, int portion) {
+    public static int portionMonth(int dogFood, int catFood, int portion) {
         return ((catFood + dogFood) / portion);
 
 
